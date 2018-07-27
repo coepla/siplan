58ede99bf32592d2d67e1f9ff41f2f4a80fc1468
martinsillo <martinsillo.gamboa@gmail.com>
2018-07-27 10:59:57 -0500

    nuevos archivos

---
+++ views/planeacion/lista_indicadores_pp.php
@@ -0,0 +1,12 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        SIPLAN 2019<br>
+        <small> Programas Presupuestarios - Indicadores Fin y Propósito</small>
+      </h1>
+    </section>
+    <section class="content">
+        lista de indicadores
+    </section>
+</div>
